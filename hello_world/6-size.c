02ce18084e7c34967c0a663af9a3bc9fbec51755
Eliot Pinto <eliotpintor@gmail.com>
2023-06-20 13:23:38 -0700

    size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,7 +11,8 @@ int main(void)
 {
 	  printf("Size of int: %lu\n", sizeof(int));
 	  printf("Size of char: %lu\n", sizeof(char));
-	  printf("Size of float: %lu\n", sizeof(float));
-	  printf("Size of double: %lu\n", sizeof(double));
+	  printf("Size of float: %d\n", sizeof(float));
+	  printf("Size of long: %lu\n", sizeof(long int));
+	  printf("Size of long long: %lu\n", sizeof(long long int));
 	return (0);
 }
