d830594b09f810bfffb8ee0bbec9415e0b91ff0c
Eliot Pinto <eliotpintor@gmail.com>
2023-06-20 13:18:26 -0700

    size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
-*  main - Prints quote
-*  
-*  Return: zero on success
-*  
+* main - Prints quote
+*
+* Return: zero on success
+*
 **/
 
-int main() {
+int main(void)
+{
 	  printf("Size of int: %d\n", sizeof(int));
 	  printf("Size of char: %d\n", sizeof(char));
 	  printf("Size of float: %d\n", sizeof(float));
 	  printf("Size of double: %d\n", sizeof(double));
-	return 0;
+	return (0);
 }
