ea4b7a9131bc49017c70fe1cd699e99d9d544199
Eliot Pinto <eliotpintor@gmail.com>
2023-06-21 15:10:03 -0700

    skip q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+
+
+/**
+ * main - Prints alphabet
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints the alphabet in lower case
+ * Skip q and e
+ * Can only use putchar
+ *
+ * Return: 0 for success
+ **/
+
+
+int main(void)
+{
+
+	char letters = 'a';
+
+		while (letters <= 'z')
+		{
+			if (letters == 'q')
+			{
+				letters++;
+			}
+			else if (letters == 'e')
+			{
+				letters++;
+			}
+			else
+			{
+				putchar(letters);
+				letters++;
+			}
+			putchar ('\n');
+			return (0);
+		}
+}
