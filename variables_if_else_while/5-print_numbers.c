8c97f92cb4e6805af19f5bc58ac657c806daab6a
Eliot Pinto <eliotpintor@gmail.com>
2023-06-21 15:30:58 -0700

    0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 10 numbers
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints number 0-9
+ *
+ * Return: 0 for success
+ **/
+
+
+int main(void)
+{
+	char num = '0';
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar ('\n');
+	return (0);
+}
