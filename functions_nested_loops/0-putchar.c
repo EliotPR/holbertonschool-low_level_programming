ebdf164f509188b0a27f464df74ef750c410ca86
Eliot Pinto <eliotpintor@gmail.com>
2023-06-22 16:36:24 -0700

    main.h created, _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints _putchar followed by a new line.
- * @void: Empty parameter list for main.
- *
- * Return: 0 for success
+ * main - Entry Point
+ * RETURN: always 0
  *
  **/
 
 
-
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
