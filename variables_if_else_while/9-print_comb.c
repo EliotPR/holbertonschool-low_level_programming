d3995617a3a048a0ae2a4459d5713103a77acfd3
Eliot Pinto <eliotpintor@gmail.com>
2023-06-21 23:32:33 -0700

    0-9 combinations

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,26 +13,15 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
 
-	for (j = 0; j <= 9; j++)
-	{
-
-	if (i <= j)
-	{
 	putchar(i + '0');
-	putchar(j + '0');
-
-	if (i != 9 || j != 9)
-	{
 	putchar(',');
 	putchar(' ');
+
 	}
-	}
-	}
-	}
+
 	return (0);
 }
