9f8c1fa43d00d9070ae73e69d4594995b2156791
Eliot Pinto <eliotpintor@gmail.com>
2023-06-22 13:45:38 -0700

    print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar followed by a new line.
+ * @void: Empty parameter list for main.
+ *
+ * Return: 0 for success
+ *
+ **/
+
+
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
