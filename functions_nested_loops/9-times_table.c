2d75bbe571d9107d31802adf6a548f021ff9fe41
Eliot Pinto <eliotpintor@gmail.com>
2023-06-23 00:06:03 -0700

    9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table up to 9
+ *
+ * Return: void
+ **/
+
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+
+	char ch = i * j + '0';
+
+	_putchar(ch);
+	_putchar(',');
+	}
+		_putchar('\n');
+	}
+}
