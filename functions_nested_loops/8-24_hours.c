2b6469f6248e3f7cd391aaa285d08d390552a356
Eliot Pinto <eliotpintor@gmail.com>
2023-06-22 23:54:05 -0700

    24 hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* jack_bauer - prints all minutes of the day
+*
+* @void: first int to add
+*
+**/
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+	for (minute = 0; minute <= 59; minute++)
+	{
+		printf("%02d:%02d\n", hour, minute);
+	}
+	}
+}
