ac0d1f9cb78f55fb6104d82b91d3c4b16dc1b653
Eliot Pinto <eliotpintor@gmail.com>
2023-06-22 17:22:40 -0700

    1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ *
+ * main - Print the alphabet
+ *
+ * Description: Print the alphabet from 'a' to 'z'.
+ *
+ * Return: 0 for success
+ *
+ *
+ **/
+
+
+void print_alphabet(void)
+{
+
+       	    char c;
+	    
+	    for (c = 'a'; c <= 'z'; c++)
+	    {
+		_putchar(c);
+	    }
+	      	_putchar('\n');
+
+}
