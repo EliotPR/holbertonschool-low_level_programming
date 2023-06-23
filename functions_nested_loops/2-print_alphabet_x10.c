5fcc0c36237ba3dbeb32be17a72b7bbeef2028de
Eliot Pinto <eliotpintor@gmail.com>
2023-06-22 19:39:00 -0700

    alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet(void)
 {
 
 	char i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
-	{				
+	{
 		_putchar(i);
-	}								
+	}
 		_putchar('\n');
 }
 
@@ -27,7 +27,7 @@ void print_alphabet(void)
 void print_alphabet_x10(void)
 {
 		int i;
-		
+
 		for (i = '0'; i <= '9'; i++)
 		{
 			print_alphabet();
