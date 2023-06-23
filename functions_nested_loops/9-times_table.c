3264c1ed73d9562f431d7586f1f1be11a1e0172e
Eliot Pinto <eliotpintor@gmail.com>
2023-06-23 00:17:41 -0700

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * times_table - prints times table up to 9
@@ -13,12 +14,15 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
+
 	for (j = 0; j <= 9; j++)
 	{
 
 	printf("%d, ", i * j);
 
 	}
-		printf('\n');
+
+	printf("\n");
+
 	}
 }
