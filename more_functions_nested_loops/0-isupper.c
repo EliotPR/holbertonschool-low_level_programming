06e0aef392db01e30681c707fa5f8231146475ce
Eliot Pinto <eliotpintor@gmail.com>
2023-06-25 14:48:49 -0700

    is upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - checks if value is upper case
- *
- * @c: variable to be checked
- *
- * _isupper: return 1 if uppercase and 0 if not
- *
- * return: 1 if c is uppercase and 0 if not
+ * _isupper - Checks if value is upper case
+ * @c: Variable to be checked
+ * Return: 1 if c is uppercase and 0 if not
  **/
 
 
@@ -15,6 +11,8 @@
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1);
+	{
+	return (1);
+	}
 	return (0);
 }
