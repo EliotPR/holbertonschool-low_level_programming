c2b14937e9a3d1396a766db0ae60de69cba7587c
Eliot Pinto <eliotpintor@gmail.com>
2023-06-25 14:15:55 -0700

    print numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - print 0-9
  *
- * Return: nothing
+ * Return: numbers 0 to 9
  **/
 
 void print_numbers(void)
@@ -11,6 +11,6 @@ void print_numbers(void)
 	char i;
 
 	for (i = '0'; i <= '9'; i++)
-	_putchar(i);
+	_putchar(i + '0');
 	_putchar('\n');
 }
